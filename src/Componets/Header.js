c9c74c3268bfbefc657ac7ac36f33988b063f0cd
princedonga <160412994+princedonga@users.noreply.github.com>
2024-10-12 11:50:53 +0530

    upload

--- src/Componets/Header.js
+++ src/Componets/Header.js
@@ -32,36 +32,36 @@ const Header = () => {
                 <div className="container-fluid">
                     <div className="row align-items-center py-3">
                         {/* Left: Search Icon */}
-                        <div className="col-2 d-flex align-items-center">
+                        <div className="col-3 d-flex align-items-center">
                             <i className="fa-solid fa-bars icon d-lg-none  d-block" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></i>
                             {/* offcanvas menu */}
 
-                            <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-                                <div class="offcanvas-header">
-                                    <h5 class="offcanvas-title logo fs-2" id="offcanvasScrollingLabel">FABRIC DEVI</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                                <div className="offcanvas-header">
+                                    <h5 className="offcanvas-title logo fs-2" id="offcanvasScrollingLabel">FABRIC DEVI</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                                 </div>
-                                <div class="offcanvas-body">
+                                <div className="offcanvas-body">
                                     <ul className='offcanvas-menu mt-4'>
                                         <li><Link>NEW ARRIVALS</Link></li>
                                         <li><Link>BEST SELLER</Link></li>
-                                        <li><Link data-bs-toggle="collapse" to="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"> PRINTED FABRIC <i class="fa-solid fa-caret-down float-end"></i></Link>
-                                            <div class="collapse" id="collapseExample">
+                                        <li><Link data-bs-toggle="collapse" to="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"> PRINTED FABRIC <i className="fa-solid fa-caret-down float-end"></i></Link>
+                                            <div className="collapse" id="collapseExample">
                                                 <div className='text-dark' >
                                                     <ul className='offcanvas-sub-menu'>
-                                                        <li className='sub-menu-set'><Link><i class="fa-solid fa-angles-right  " style={{ letterSpacing: "10px" }}></i>Polyester Fabric</Link></li>
+                                                        <li className='sub-menu-set'><Link><i className="fa-solid fa-angles-right  " style={{ letterSpacing: "8px" }}></i>Polyester Fabric</Link></li>
                                                         <li><Link>- All</Link></li>
                                                         <li><Link>- Japan Satin</Link></li>
                                                         <li><Link>- Tusser Silk</Link></li>
                                                     </ul>
                                                     <ul className='offcanvas-sub-menu'>
-                                                        <li className='sub-menu-set'><Link><i class="fa-solid fa-angles-right  " style={{ letterSpacing: "10px" }}></i>Polyester Fabric</Link></li>
+                                                        <li className='sub-menu-set'><Link><i className="fa-solid fa-angles-right  " style={{ letterSpacing: "8px" }}></i>Polyester Fabric</Link></li>
                                                         <li><Link>- All</Link></li>
                                                         <li><Link>- Japan Satin</Link></li>
                                                         <li><Link>- Tusser Silk</Link></li>
                                                     </ul>
                                                     <ul className='offcanvas-sub-menu'>
-                                                        <li className='sub-menu-set'><Link><i class="fa-solid fa-angles-right  " style={{ letterSpacing: "10px" }}></i>Polyester Fabric</Link></li>
+                                                        <li className='sub-menu-set'><Link><i className="fa-solid fa-angles-right  " style={{ letterSpacing: "8px" }}></i>Polyester Fabric</Link></li>
                                                         <li><Link>- All</Link></li>
                                                         <li><Link>- Japan Satin</Link></li>
                                                         <li><Link>- Tusser Silk</Link></li>
@@ -77,16 +77,36 @@ const Header = () => {
 
                             {/* offcanvas menu */}
 
-                            <i className="fa-solid fa-magnifying-glass icon"></i>
+                            <i className="fa-solid fa-magnifying-glass icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"></i>
+
+                            {/* SearchBar */}
+
+
+                            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExamicon1pleLabel">
+                                <div className="offcanvas-header">
+                                    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div className="offcanvas-body">
+                                    <div className="search-box">
+                                        <input type="text" name="" id="" placeholder="search..."/>
+                                            <Link to="##" className="icon1">
+                                                <i className="fas fa-search"></i>
+                                            </Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* SearchBar */}
                         </div>
 
                         {/* Center: Logo */}
-                        <div className="col-8 text-center">
+                        <div className="col-6 text-center">
                             <h2 className="logo">FABRIC DEVI</h2>
                         </div>
 
                         {/* Right: User, Wishlist, Cart Icons */}
-                        <div className="col-2 d-flex justify-content-end align-items-center">
+                        <div className="col-3 d-flex justify-content-end align-items-center">
                             <i className="fa-regular fa-user icon"></i>
                             <div className="wishlist">
                                 <i className="fa-regular fa-heart icon"></i>
