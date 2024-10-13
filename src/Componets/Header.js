653f89fcd6b422cc08d6a0aa9abb5ffd9d96a17c
princedonga <160412994+princedonga@users.noreply.github.com>
2024-10-13 17:46:37 +0530

    upload

--- src/Componets/Header.js
+++ src/Componets/Header.js
@@ -2,17 +2,16 @@ import React, { useState } from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
     const [showModal, setShowModal] = useState(false);
 
     const toggleModal = () => {
         setShowModal(!showModal);
     };
-
     return (
         <div>
             <div className='bg-dark py-3'>
-                {/* Carousel */}
                 <div id="carouselExample" className="carousel slide text-center text-white" data-bs-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
@@ -25,8 +24,8 @@ const Header = () => {
                             <span>More Than 500+ customers</span>
                         </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <button className="carousel-control-prev " type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon " aria-hidden="true"></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
                     <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
@@ -35,17 +34,74 @@ const Header = () => {
                     </button>
                 </div>
             </div>
-
-            {/* Main Header */}
             <header className="py-2 container">
                 <div className="container-fluid">
                     <div className="row align-items-center py-3">
                         {/* Left: Search Icon */}
                         <div className="col-3 d-flex align-items-center">
-                            <i className="fa-solid fa-bars icon d-lg-none d-block" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></i>
+                            <i className="fa-solid fa-bars icon d-lg-none  d-block" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></i>
+                            {/* offcanvas menu */}
 
-                            {/* Search Icon */}
+                            <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                                <div className="offcanvas-header">
+                                    <h5 className="offcanvas-title logo fs-2" id="offcanvasScrollingLabel">FABRIC DEVI</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div className="offcanvas-body">
+                                    <ul className='offcanvas-menu mt-4'>
+                                        <li><Link>NEW ARRIVALS</Link></li>
+                                        <li><Link>BEST SELLER</Link></li>
+                                        <li><Link data-bs-toggle="collapse" to="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"> PRINTED FABRIC <i className="fa-solid fa-caret-down float-end"></i></Link>
+                                            <div className="collapse" id="collapseExample">
+                                                <div className='text-dark' >
+                                                    <ul className='offcanvas-sub-menu'>
+                                                        <li className='sub-menu-set'><Link><i className="fa-solid fa-angles-right  " style={{ letterSpacing: "8px" }}></i>Polyester Fabric</Link></li>
+                                                        <li><Link>- All</Link></li>
+                                                        <li><Link>- Japan Satin</Link></li>
+                                                        <li><Link>- Tusser Silk</Link></li>
+                                                    </ul>
+                                                    <ul className='offcanvas-sub-menu'>
+                                                        <li className='sub-menu-set'><Link><i className="fa-solid fa-angles-right  " style={{ letterSpacing: "8px" }}></i>Polyester Fabric</Link></li>
+                                                        <li><Link>- All</Link></li>
+                                                        <li><Link>- Japan Satin</Link></li>
+                                                        <li><Link>- Tusser Silk</Link></li>
+                                                    </ul>
+                                                    <ul className='offcanvas-sub-menu'>
+                                                        <li className='sub-menu-set'><Link><i className="fa-solid fa-angles-right  " style={{ letterSpacing: "8px" }}></i>Polyester Fabric</Link></li>
+                                                        <li><Link>- All</Link></li>
+                                                        <li><Link>- Japan Satin</Link></li>
+                                                        <li><Link>- Tusser Silk</Link></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li><Link>FABRIC SET</Link></li>
+                                        <li><Link>SALE</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            {/* offcanvas menu */}
+
                             <i className="fa-solid fa-magnifying-glass icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"></i>
+
+                            {/* SearchBar */}
+
+
+                            <div className="offcanvas offcanvas-end" style={{minWidth:"37%"}} tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExamicon1pleLabel">
+                                <div className="offcanvas-header">
+                                    <h6 className="offcanvas-title" id="offcanvasExampleLabel" style={{color:"#ccc"}}>WHAT ARE YOU LOOKIN FOR ?</h6>
+                                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div className="offcanvas-body">
+                                    <div class="search-container">
+                                        <input type="text" placeholder="Search Products..." />
+                                        <span class="search-icon"><i class="fa-solid fa-magnifying-glass"></i></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* SearchBar */}
                         </div>
 
                         {/* Center: Logo */}
@@ -73,13 +129,36 @@ const Header = () => {
                             <nav className="navbar navbar-expand-lg">
                                 <ul className="navbar-nav mx-auto">
                                     <li className="nav-item"><Link to="" className="nav-link">NEW ARRIVALS</Link></li>
-                                    <li className="nav-item"><Link to="" className="nav-link">BEST SELLER</Link></li>
+                                    <li className="nav-item">
+                                        <Link to="" className="nav-link" >BEST SELLER</Link>
+                                    </li>
+                                    {/* PRINTED FABRIC with Dropdown */}
                                     <li className="nav-item dropdown">
                                         <Link to="" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                                             PRINTED FABRIC
                                         </Link>
+                                        {/* Dropdown menu */}
                                         <div className="dropdown-menu">
-                                            {/* Dropdown columns */}
+                                            <div className="dropdown-column">
+                                                <h6 className="dropdown-header">Polyester Fabric</h6>
+                                                <Link className="dropdown-item" to="#">All</Link>
+                                                <Link className="dropdown-item" to="#">Japan Satin</Link>
+                                                <Link className="dropdown-item" to="#">Tusser Silk</Link>
+                                                {/* Add more items here */}
+                                            </div>
+                                            <div className="dropdown-column">
+                                                <h6 className="dropdown-header">Pure & Viscose Fabric</h6>
+                                                <Link className="dropdown-item" to="#">All</Link>
+                                                <Link className="dropdown-item" to="#">Pure Cotton</Link>
+                                                <Link className="dropdown-item" to="#">Pure Chiffon</Link>
+                                                {/* Add more items here */}
+                                            </div>
+                                            <div className="dropdown-column">
+                                                <h6 className="dropdown-header">Fabric Quality</h6>
+                                                <Link className="dropdown-item" to="#">Pure Fabric</Link>
+                                                <Link className="dropdown-item" to="#">Viscose Fabric</Link>
+                                                {/* Add more items here */}
+                                            </div>
                                         </div>
                                     </li>
                                     <li className="nav-item"><Link to="" className="nav-link">FABRIC SET</Link></li>
@@ -90,14 +169,12 @@ const Header = () => {
                     </div>
                 </div>
             </header>
-
-            {/* Modal for login */}
             {showModal && (
                 <div className="modal fade show d-block" tabIndex="-1" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }} role="dialog">
                     <div className="modal-dialog modal-dialog-centered" role="document">
                         <div className="modal-content">
                             <div className="modal-header border-bottom-0">
-                                <h5 className="modal-title w-100 text-center">Login</h5>
+                                <h3 className="modal-title w-100 text-center">Login</h3>
                                 <button type="button" className="btn-close" onClick={toggleModal}></button>
                             </div>
                             <div className="modal-body">
